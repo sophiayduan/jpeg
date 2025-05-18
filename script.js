f17734650e2965531b42371572bb420dc177bbc5
haya raham asif <haya.raham-asif@stu.ocsb.ca>
2025-05-18 00:16:02 +0000

    added rope and battery scroll

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("scroll", function () {
+  let maxHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let userScrollPos = window.scrollY;
+  let widthBasedOnScrollPercentage = 7.5 - ((userScrollPos / maxHeight) * 7.5); // calulcating what fraction of the webpage the user has scrolled thorugh
+  document.getElementById(
+    "progress-bar"
+  ).style.width = `${widthBasedOnScrollPercentage}vw`;
+})
