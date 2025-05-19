d7224a07c04e74d98983ae688f3e13b9b1a8df30
haya-rahamasif <49449681+aquaseals@users.noreply.github.com>
2025-05-19 02:40:02 +0000

    cleaned up styles + added free banner

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ document.addEventListener("scroll", function () {
     document.documentElement.scrollHeight -
     document.documentElement.clientHeight;
   let userScrollPos = window.scrollY;
-  let widthBasedOnScrollPercentage = 7.5 - ((userScrollPos / maxHeight) * 7.5); // calulcating what fraction of the webpage the user has scrolled thorugh
+  let maxIMGWidth = ((document.getElementById('battery').clientWidth/document.documentElement.clientWidth)*100)/1.65
+  let widthBasedOnScrollPercentage = maxIMGWidth - ((userScrollPos / maxHeight) * maxIMGWidth); // calulcating what fraction of the webpage the user has scrolled thorugh
+  console.log(widthBasedOnScrollPercentage)
   document.getElementById(
     "progress-bar"
   ).style.width = `${widthBasedOnScrollPercentage}vw`;
